48b7f282557e96164882833361b1fbf03e292aff
Pakistan Law Reports <304617415+pakistanlawreports@users.noreply.github.com>
2026-07-14 14:59:44 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,2 +1,1 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0555529856361728"
-     crossorigin="anonymous"></script>
+<meta name="google-adsense-account" content="ca-pub-0555529856361728">
